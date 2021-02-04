8a660cf90878ed4624bde5077c0a8a73e215ef34
Anmol Sonkar <68446881+AnmolSonkar@users.noreply.github.com>
2021-02-04 12:28:47 +0530

    Update Slideshow.php

--- Slideshow.php
+++ Slideshow.php
@@ -10,13 +10,13 @@
       <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
     </ol>
     <div class="carousel-inner">
-      <div class="carousel-item active"> <img class="d-block w-100 " src="IMG/A.webp" id="Slideshowimage" alt="First slide"> </div>
-      <div class="carousel-item"> <img class="d-block w-100" src="IMG/B.webp" id="Slideshowimage" alt="Second slide"> </div>
-      <div class="carousel-item"> <img class="d-block w-100" src="IMG/C.webp" id="Slideshowimage" alt="Third slide"> </div>
-      <div class="carousel-item"> <img class="d-block w-100" src="IMG/D.webp" id="Slideshowimage" alt="Fourth slide"> </div>
-      <div class="carousel-item"> <img class="d-block w-100" src="IMG/E.webp" id="Slideshowimage" alt="Fifth slide"> </div>
-      <div class="carousel-item"> <img class="d-block w-100" src="IMG/F.webp" id="Slideshowimage" alt="Sixth slide"> </div>
-      <div class="carousel-item"> <img class="d-block w-100" src="IMG/G.webp" id="Slideshowimage" alt="Seventh slide"> </div>
+      <div class="carousel-item active"> <img class="d-block w-100 " src="IMG/Carousel/A.webp" id="Slideshowimage" alt="First slide"> </div>
+      <div class="carousel-item"> <img class="d-block w-100" src="IMG/Carousel/B.webp" id="Slideshowimage" alt="Second slide"> </div>
+      <div class="carousel-item"> <img class="d-block w-100" src="IMG/Carousel/C.webp" id="Slideshowimage" alt="Third slide"> </div>
+      <div class="carousel-item"> <img class="d-block w-100" src="IMG/Carousel/D.webp" id="Slideshowimage" alt="Fourth slide"> </div>
+      <div class="carousel-item"> <img class="d-block w-100" src="IMG/Carousel/E.webp" id="Slideshowimage" alt="Fifth slide"> </div>
+      <div class="carousel-item"> <img class="d-block w-100" src="IMG/Carousel/F.webp" id="Slideshowimage" alt="Sixth slide"> </div>
+      <div class="carousel-item"> <img class="d-block w-100" src="IMG/Carousel/G.webp" id="Slideshowimage" alt="Seventh slide"> </div>
     </div>
     <div class="d-lg-none d-sm-none">
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a>
